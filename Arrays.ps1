5761bcab08078b329a96546fe3d79f149c48bd34
robin <purirobin18@gmail.com>
2024-09-07 12:26:42 +0530

    changes were made in array

---
+++ Arrays.ps1
@@ -0,0 +1,30 @@
+# In PowerShell, arrays are used to store collections of elements, and they allow you to work with
+# multiple values in a single variable.
+# Here's how arrays work in PowerShell,
+# including how to declare, access, manipulate, and perform operations on arrays.
+
+# ----------------------------------------------------------------
+$prInt = " Declaring a an array of integers"
+
+$myArrayint = 1,2,3,4,5,6,7,8,9
+$prInt
+$myArrayint
+
+# -----------------------------------------------------------------------
+
+$prIntstring = "decalring a an array of string"
+
+$myStringArray = "Apple", "Banana", "Cherry"
+
+$prIntstring
+$myStringArray
+
+
+# ------------------------------------------------------------------
+$prIntMixed = "decalring a an array of string and int "
+
+$prIntMixed
+
+$myArray =@("hello","hello2","hello3",10,20)
+
+$myArray
