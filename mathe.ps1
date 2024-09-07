c4f6e6480b1dee1babd6ecd36276ba359fba668b
Robin <112844175+IsItRobin@users.noreply.github.com>
2024-09-07 12:08:29 +0530

    Add files via upload

---
+++ mathe.ps1
@@ -0,0 +1,62 @@
+# performing a simple addition 
+# following cammel rules
+# subtraction - addition -division-multiplication-modules
+
+$myInt = 20
+# our first variable for integer
+$myInt2 =30
+# our seecand variiable for integer
+
+$myRes = $myInt+$myInt2
+# printing the result
+$myRes
+
+
+# now using whole number
+$myDo = 20.1
+# our first variable for double
+$myDo2 =30.2
+# our seecand variiable for double
+
+$myResDo = $myDO+$myDO2
+# printing the result
+$myResDo
+
+# ------------------------------subtraction--------------------------------------
+$myInt = 30
+# our first variable for integer
+$myInt2 =10
+# our seecand variiable for integer
+
+$myRes = $myInt-$myInt2
+# printing the result
+$myRes
+# ------------------------------multiplication--------------------------------------
+$myInt = 30
+# our first variable for integer
+$myInt2 =10
+# our seecand variiable for integer
+
+$myResmul = $myInt * $myInt2
+# printing the result
+$myResmul
+# ------------------------------Division--------------------------------------
+$myInt = 30
+# our first variable for integer
+$myInt2 =10
+# our seecand variiable for integer
+
+$myResdiv = $myInt / $myInt2
+# printing the result
+$myResdiv
+
+# ------------------------------modules--------------------------------------
+$myInt = 30
+# our first variable for integer
+$myInt2 =10
+# our seecand variiable for integer
+
+$myResmod = $myInt % $myInt2
+# printing the result
+$myResmod
+
---
+++ variables.ps1
@@ -0,0 +1,28 @@
+# to declare a variable we need to $ sign
+# the variable should follow cammel casing
+
+
+$myString1 = "Robin" # to delcare a string we will use "",'' 
+
+$myString1# to printout the string 1
+
+
+# to checkwhat type of varable it is we use      variablename.Gettype()
+$myString1.GetType()
+
+# for intiger
+$myVarInt = 25
+
+$myVarInt
+
+$myVarInt.GetType()
+
+
+# for double
+$myVarD =12.1
+$myVarD.GetType()
+
+# lets perform addition
+
+$myRes = $myVarInt + $myVarD
+$myRes
