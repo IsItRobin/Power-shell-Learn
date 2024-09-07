9ef16a99553eb57de9e3875a832838d94a9d9b08
Robin <112844175+IsItRobin@users.noreply.github.com>
2024-09-07 12:10:33 +0530

    Update mathe.ps1

--- mathe.ps1
+++ mathe.ps1
@@ -1,62 +1,112 @@
-# performing a simple addition 
-# following cammel rules
-# subtraction - addition -division-multiplication-modules
-
+# ------------------------------Addition--------------------------------------
 $myInt = 20
-# our first variable for integer
-$myInt2 =30
-# our seecand variiable for integer
+# Our first variable for integer
+$myInt2 = 30
+# Our second variable for integer
 
-$myRes = $myInt+$myInt2
-# printing the result
+$myRes = $myInt + $myInt2
+# Performing addition and storing the result in $myRes
 $myRes
+# Printing the result of addition
 
-
-# now using whole number
+# Now using floating-point (double) numbers
 $myDo = 20.1
-# our first variable for double
-$myDo2 =30.2
-# our seecand variiable for double
+# Our first variable for double
+$myDo2 = 30.2
+# Our second variable for double
 
-$myResDo = $myDO+$myDO2
-# printing the result
+$myResDo = $myDo + $myDo2
+# Performing addition with double values
 $myResDo
+# Printing the result of addition with doubles
 
-# ------------------------------subtraction--------------------------------------
+# ------------------------------Subtraction--------------------------------------
 $myInt = 30
-# our first variable for integer
-$myInt2 =10
-# our seecand variiable for integer
+# Our first variable for integer
+$myInt2 = 10
+# Our second variable for integer
 
-$myRes = $myInt-$myInt2
-# printing the result
+$myRes = $myInt - $myInt2
+# Performing subtraction and storing the result in $myRes
 $myRes
-# ------------------------------multiplication--------------------------------------
-$myInt = 30
-# our first variable for integer
-$myInt2 =10
-# our seecand variiable for integer
+# Printing the result of subtraction
 
-$myResmul = $myInt * $myInt2
-# printing the result
-$myResmul
+# ------------------------------Multiplication--------------------------------------
+$myInt = 30
+# Our first variable for integer
+$myInt2 = 10
+# Our second variable for integer
+
+$myResMul = $myInt * $myInt2
+# Performing multiplication and storing the result in $myResMul
+$myResMul
+# Printing the result of multiplication
+
 # ------------------------------Division--------------------------------------
 $myInt = 30
-# our first variable for integer
-$myInt2 =10
-# our seecand variiable for integer
+# Our first variable for integer
+$myInt2 = 10
+# Our second variable for integer
 
-$myResdiv = $myInt / $myInt2
-# printing the result
-$myResdiv
+$myResDiv = $myInt / $myInt2
+# Performing division and storing the result in $myResDiv
+$myResDiv
+# Printing the result of division
 
-# ------------------------------modules--------------------------------------
+# ------------------------------Modulus (Remainder)--------------------------------------
 $myInt = 30
-# our first variable for integer
-$myInt2 =10
-# our seecand variiable for integer
+# Our first variable for integer
+$myInt2 = 10
+# Our second variable for integer
 
-$myResmod = $myInt % $myInt2
-# printing the result
-$myResmod
+$myResMod = $myInt % $myInt2
+# Performing modulus operation and storing the result in $myResMod (remainder of division)
+$myResMod
+# Printing the result of modulus operation
 
+# ------------------------------Exponentiation--------------------------------------
+$myBase = 5
+# Base number
+$myExponent = 3
+# Exponent to raise the base to
+
+$myResExp = [math]::Pow($myBase, $myExponent)
+# Performing exponentiation (5 raised to the power of 3)
+$myResExp
+# Printing the result of exponentiation
+
+# ------------------------------Increment--------------------------------------
+$myValue = 10
+# Initial value
+
+$myValue++
+# Incrementing the value by 1 (myValue = 11)
+$myValue
+# Printing the incremented value
+
+# ------------------------------Decrement--------------------------------------
+$myValue = 15
+# Initial value
+
+$myValue--
+# Decrementing the value by 1 (myValue = 14)
+$myValue
+# Printing the decremented value
+
+# ------------------------------Floor Division--------------------------------------
+$myFloat = 22.7
+# Floating-point number
+
+$myResFloorDiv = [math]::Floor($myFloat / 4)
+# Performing floor division (rounding down the division result)
+$myResFloorDiv
+# Printing the result of floor division
+
+# ------------------------------Square Root--------------------------------------
+$myNumber = 16
+# Number to find the square root of
+
+$myResSqrt = [math]::Sqrt($myNumber)
+# Performing square root operation
+$myResSqrt
+# Printing the result of the square root
